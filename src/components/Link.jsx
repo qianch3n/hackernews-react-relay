cd12cf14928c58dc8c66cf32746c36ce37d1ae31
Qianchen <qianchen.he@nordstrom.com>
2018-08-16 16:55:16 -0700

    update

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,4 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+  createFragmentContainer,
+  graphql
+} from 'react-relay';
 
 class Link extends Component {
 
@@ -16,4 +20,10 @@ class Link extends Component {
 
 }
 
-export default Link
+export default createFragmentContainer(Link, graphql`
+  fragment Link_link on Link {
+    id
+    description
+    url
+  }
+`)
